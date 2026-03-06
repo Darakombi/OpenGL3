2f5629eaf5c3dd699c9559d8abe94223ac7ef2fb
Darakombi <Darakombi@gmail.com>
2026-03-06 23:04:01 +0200

    Model loading

--- OpenGL/source/Mesh.h
+++ OpenGL/source/Mesh.h
@@ -1,37 +1,96 @@
-#pragma once
+#ifndef MESH_H
+#define MESH_H
 
 #include "Shader.h"
 
 #include "glm/glm.hpp"
 
-#include <iostream>
+#include "string"
 #include <vector>
 
-struct MeshVertex {
+struct Vertex {
 	glm::vec3 Position;
 	glm::vec3 Normal;
 	glm::vec2 TexCoord;
 };
 
-struct MeshTexture {
+struct Texture {
 	unsigned int id;
 	std::string type;
+	std::string path;
 };
 
 class Mesh {
 
 public:
-	std::vector<MeshVertex> Vertices;
+	std::vector<Vertex> Vertices;
 	std::vector<unsigned int> Indices;
-	std::vector<MeshTexture> Textures;
+	std::vector<Texture> Textures;
 
-	Mesh(const std::vector<MeshVertex> vertices,
-		const std::vector<unsigned int> indices,
-		const std::vector<MeshTexture> textures);
+	Mesh(const std::vector<Vertex> vertices,
+		 const std::vector<unsigned int> indices,
+		 const std::vector<Texture> textures)
+		: Vertices(vertices),
+		Indices(indices),
+		Textures(textures)
+	{
+		setupMesh();
+	}
 
-	void Draw(Shader& shader) const;
+	void Draw(const Shader& shader) const {
+		unsigned int diffuseArg = 1;
+		unsigned int specularArg = 1;
+
+		for (int i = 0; i < Textures.size(); i++) {
+
+			glActiveTexture(GL_TEXTURE0 + i);
+
+			std::string arg;
+			const std::string type = Textures[i].type;
+			if (type == "diffuse") {
+				arg = std::to_string(diffuseArg++);
+			}
+			else if (type == "specular") {
+				arg = std::to_string(specularArg++);
+			}
+
+			std::string name = "tex_" + type + arg;
+
+			shader.U1i(name.c_str(), i);
+			glBindTexture(GL_TEXTURE_2D, Textures[i].id);
+		}
+
+		glBindVertexArray(VAO);
+		glDrawElements(GL_TRIANGLES, static_cast<unsigned int>(Indices.size()), GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		glActiveTexture(GL_TEXTURE0);
+	}
 
 private:
 	unsigned int VAO, IBO, VBO;
-	void setupMesh();
+
+	void setupMesh() {
+		glGenVertexArrays(1, &VAO);
+		glBindVertexArray(VAO);
+
+		glGenBuffers(1, &VBO);
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferData(GL_ARRAY_BUFFER, Vertices.size() * sizeof(Vertex), Vertices.data(), GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
+		glEnableVertexAttribArray(0);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
+		glEnableVertexAttribArray(1);
+		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoord));
+		glEnableVertexAttribArray(2);
+
+		glGenBuffers(1, &IBO);
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, Indices.size() * sizeof(unsigned int), &Indices[0], GL_STATIC_DRAW);
+
+		glBindVertexArray(0);
+	}
+
 };
+
+#endif
--- OpenGL/source/main.cpp
+++ OpenGL/source/main.cpp
@@ -9,14 +9,8 @@
 #include <iostream>
 #include <string>
 
-#include "Object.h"
-#include "VertexArray.h"
-#include "VertexBuffer.h"
-#include "VertexBufferLayout.h"
-#include "IndexBuffer.h"
 #include "Shader.h"
-#include "Texture.h"
-
+#include "Model.h"
 #include "Camera.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -26,9 +20,7 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 
 void processInput(GLFWwindow* window);
 
-inline static const float time() { return (float)glfwGetTime(); }
-
-const unsigned int SCR_WIDTH = 1200;
+const unsigned int SCR_WIDTH = 1000;
 const unsigned int SCR_HEIGHT = 800;
 
 float deltaTime = 0.0f;
@@ -50,7 +42,7 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Lookin for my Kamakiri", nullptr, nullptr);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Looking for my Kamakiri", nullptr, nullptr);
 	if (window == NULL) {
 		std::cout << "Failed to initialize window" << std::endl;
 		glfwTerminate();
@@ -64,6 +56,10 @@ int main() {
 	}
 
 	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glEnable(GL_DEPTH_TEST);
+
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwSetCursorPosCallback(window, mouse_callback);
@@ -71,151 +67,12 @@ int main() {
 	glfwSetMouseButtonCallback(window, mouse_button_callback);
 
 	glfwSwapInterval(1);
-	glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glEnable(GL_DEPTH_TEST);
+	stbi_set_flip_vertically_on_load(true);
 
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	const float subjectVertices[] = {
-		// back face
-		-0.5, -0.5, -0.5,	 0,  0, -1,		0, 0,
-		 0.5,  0.5, -0.5,	 0,  0, -1,		1, 1,
-		 0.5, -0.5, -0.5,	 0,  0, -1,		1, 0,
-		 0.5,  0.5, -0.5,	 0,  0, -1,		1, 1,
-		-0.5, -0.5, -0.5,	 0,  0, -1,		0, 0,
-		-0.5,  0.5, -0.5,	 0,  0, -1,		0, 1,
-
-		// front face
-		-0.5, -0.5,  0.5,	 0,  0,  1,		0, 0,
-		 0.5, -0.5,  0.5,	 0,  0,  1,		1, 0,
-		 0.5,  0.5,  0.5,	 0,  0,  1,		1, 1,
-		 0.5,  0.5,  0.5,	 0,  0,  1,		1, 1,
-		-0.5,  0.5,  0.5,	 0,  0,  1,		0, 1,
-		-0.5, -0.5,  0.5,	 0,  0,  1,		0, 0,
-
-		// left face			 	 
-		-0.5, -0.5, -0.5,	-1,  0,  0,		0, 0,
-		-0.5,  0.5, -0.5,	-1,  0,  0,		0, 1,
-		-0.5,  0.5,  0.5,	-1,  0,  0,		1, 1,
-		-0.5, -0.5, -0.5,	-1,  0,  0,		0, 0,
-		-0.5,  0.5,  0.5,	-1,  0,  0,		1, 1,
-		-0.5, -0.5,  0.5,	-1,  0,  0,		1, 0,
-
-		// right face			 	 
-		 0.5, -0.5, -0.5,	 1,  0,  0,		1, 0,
-		 0.5,  0.5, -0.5,	 1,  0,  0,		1, 1,
-		 0.5,  0.5,  0.5,	 1,  0,  0,		0, 1,
-		 0.5, -0.5, -0.5,	 1,  0,  0,		1, 0,
-		 0.5, -0.5,  0.5,	 1,  0,  0,		0, 0,
-		 0.5,  0.5,  0.5,	 1,  0,  0,		0, 1,
-
-		 // bottom face			 	 
-		-0.5, -0.5,  0.5,	 0, -1,  0,		0, 1,
-		 0.5, -0.5,  0.5,	 0, -1,  0,		1, 1,
-		 0.5, -0.5, -0.5,	 0, -1,  0,		1, 0,
-		 0.5, -0.5, -0.5,	 0, -1,  0,		1, 0,
-		-0.5, -0.5, -0.5,	 0, -1,  0,		0, 0,
-		-0.5, -0.5,  0.5,	 0, -1,  0,		0, 1,
-
-		// top face
-		-0.5,  0.5, -0.5,	 0,  1,  0,		0, 0,
-		 0.5,  0.5, -0.5,	 0,  1,  0,		1, 0,
-		 0.5,  0.5,  0.5,	 0,  1,  0,		1, 1,
-		-0.5,  0.5, -0.5,	 0,  1,  0,		0, 0,
-		-0.5,  0.5,  0.5,	 0,  1,  0,		0, 1,
-		 0.5,  0.5,  0.5,	 0,  1,  0,		1, 1
-	};
-	const unsigned int subjectLayout[] = { 3, 3, 2 };
-	Object subject(subjectVertices, sizeof(subjectVertices) / sizeof(float), subjectLayout, sizeof(subjectLayout) / sizeof(unsigned int));
-	Shader subjectShader("source/resources/shaders/Main.Shader");
-	Texture subjectTexture("source/resources/textures/container.png");
-	Texture subjectTextureSpecular("source/resources/textures/containerSpecular.png");
-
-	const float lightVertices[] = {
-		// back face
-		-0.5, -0.5, -0.5,	 0, 0,
-		 0.5,  0.5, -0.5,	 1, 1,
-		 0.5, -0.5, -0.5,	 1, 0,
-		 0.5,  0.5, -0.5,	 1, 1,
-		-0.5, -0.5, -0.5,	 0, 0,
-		-0.5,  0.5, -0.5,	 0, 1,
-
-		// front face
-		-0.5, -0.5,  0.5,	 0, 0,
-		 0.5, -0.5,  0.5,	 1, 0,
-		 0.5,  0.5,  0.5,	 1, 1,
-		 0.5,  0.5,  0.5,	 1, 1,
-		-0.5,  0.5,  0.5,	 0, 1,
-		-0.5, -0.5,  0.5,	 0, 0,
-
-		// left face		
-		-0.5, -0.5, -0.5,	-0, 0,
-		-0.5,  0.5, -0.5,	-0, 1,
-		-0.5,  0.5,  0.5,	-1, 1,
-		-0.5, -0.5, -0.5,	-0, 0,
-		-0.5,  0.5,  0.5,	-1, 1,
-		-0.5, -0.5,  0.5,	-1, 0,
-
-		// right face		
-		 0.5, -0.5, -0.5,	 1, 0,
-		 0.5,  0.5, -0.5,	 1, 1,
-		 0.5,  0.5,  0.5,	 0, 1,
-		 0.5, -0.5, -0.5,	 1, 0,
-		 0.5, -0.5,  0.5,	 0, 0,
-		 0.5,  0.5,  0.5,	 0, 1,
-
-		 // bottom face		
-		-0.5, -0.5,  0.5,	 0, 1,
-		 0.5, -0.5,  0.5,	 1, 1,
-		 0.5, -0.5, -0.5,	 1, 0,
-		 0.5, -0.5, -0.5,	 1, 0,
-		-0.5, -0.5, -0.5,	 0, 0,
-		-0.5, -0.5,  0.5,	 0, 1,
-
-		// top face
-		-0.5,  0.5, -0.5,	 0, 0,
-		 0.5,  0.5, -0.5,	 1, 0,
-		 0.5,  0.5,  0.5,	 1, 1,
-		-0.5,  0.5, -0.5,	 0, 0,
-		-0.5,  0.5,  0.5,	 0, 1,
-		 0.5,  0.5,  0.5,	 1, 1
-	};
-	const unsigned int lightLayout[] = { 3, 2 };
-	Object light(lightVertices, sizeof(lightVertices) / sizeof(float), lightLayout, sizeof(lightLayout) / sizeof(unsigned int));
-	Shader lightShader("source/resources/shaders/Lamp.Shader");
-	Texture lightTexture("source/resources/textures/glowstone.png");
-
-	glm::vec3 lightPos(0.0f, 0.0f, -7.0f);
-	glm::vec3 lightDir(-0.2f, -1.0f, -0.3f);
-	glm::vec3 lightColor(1.0f, 1.0f, 1.0f);
-
-	float shininess = 32.0f;
-
-	glm::vec3 ambientStrength(0.05f);
-	glm::vec3 diffuseStrength(0.8f);
-	glm::vec3 specularStrength(1.0f);
-
-	glm::vec3 cubePositions[] = {
-		glm::vec3(0.0f,  0.0f,  0.0f),
-		glm::vec3(2.0f,  5.0f, -15.0f),
-		glm::vec3(-1.5f, -2.2f, -2.5f),
-		glm::vec3(-3.8f, -2.0f, -12.3f),
-		glm::vec3(2.4f, -0.4f, -3.5f),
-		glm::vec3(-1.7f,  3.0f, -7.5f),
-		glm::vec3(1.3f, -2.0f, -2.5f),
-		glm::vec3(1.5f,  2.0f, -2.5f),
-		glm::vec3(1.5f,  0.2f, -1.5f),
-		glm::vec3(-1.3f,  1.0f, -1.5f)
-	};
-
-	glm::vec3 pointLightPositions[] = {
-		glm::vec3(0.7f,  0.2f,  2.0f),
-		//glm::vec3(2.3f, -3.3f, -4.0f),
-		glm::vec3(-4.0f,  2.0f, -12.0f),
-		//glm::vec3(0.0f,  0.0f, -3.0f),
-	};
-	unsigned int points = 2;
+	Shader shader("source/resources/shaders/Main.Shader");
+	Model testModel("source/resources/textures/backpack/backpack.obj");
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -227,81 +84,17 @@ int main() {
 
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+		shader.Bind();
+
 		glm::mat4 proj = glm::perspective(glm::radians(camera.Fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		glm::mat4 view = camera.getViewMatrix();
 		glm::mat4 model(1.0f);
 
-		{
-			subject.Bind();
-			subjectShader.Bind();
-
-			subjectTexture.Bind(0);
-			subjectShader.U1i("material.diffuse", 0);
-			subjectTextureSpecular.Bind(1);
-			subjectShader.U1i("material.specular", 1);
-			subjectShader.U1f("material.shininess", shininess);
-
-			subjectShader.U3fv("u_ViewPos", glm::value_ptr(camera.getPosition()));
-
-			subjectShader.U3f("dirLight.direction", -0.2f, -1.0f, -0.3f);
-			subjectShader.U3f("dirLight.ambient", 0.05f, 0.05f, 0.05f);
-			subjectShader.U3f("dirLight.diffuse", 0.8f, 0.8f, 0.8f);
-			subjectShader.U3f("dirLight.specular", 1.0f, 1.0f, 1.0f);
-
-			for (unsigned int i = 0; i < points; i++) {
-				std::string name = "pointLights[" + std::to_string(i) + "]";
-
-				subjectShader.U3fv((name + ".position").c_str(), glm::value_ptr(pointLightPositions[i]));
-				subjectShader.U3f((name + ".ambient").c_str(), 0.05f, 0.05f, 0.05f);
-				subjectShader.U3f((name + ".diffuse").c_str(), 0.8f, 0.8f, 0.8f);
-				subjectShader.U3f((name + ".specular").c_str(), 1.0f, 1.0f, 1.0f);
-				subjectShader.U1f((name + ".constant").c_str(), 1.0f);
-				subjectShader.U1f((name + ".linear").c_str(), 0.09f);
-				subjectShader.U1f((name + ".quadratic").c_str(), 0.032f);
-			}
-
-			subjectShader.U3fv("spotLight.position", glm::value_ptr(camera.getPosition()));
-			subjectShader.U3fv("spotLight.direction", glm::value_ptr(camera.getFront()));
-			subjectShader.U3f("spotLight.ambient", 0.0f, 0.0f, 0.0f);
-			subjectShader.U3f("spotLight.diffuse", flashlight ? 1.0f : 0.0f, flashlight ? 1.0f : 0.0f, flashlight ? 1.0f : 0.0f);
-			subjectShader.U3f("spotLight.specular", 1.0f, 1.0f, 1.0f);
-			subjectShader.U1f("spotLight.constant", 1.0f);
-			subjectShader.U1f("spotLight.linear", 0.045f);
-			subjectShader.U1f("spotLight.quadratic", 0.0075f);
-			subjectShader.U1f("spotLight.innerCutoff", cos(glm::radians(12.5f)));
-			subjectShader.U1f("spotLight.outerCutoff", cos(glm::radians(17.5f)));
-
-			subjectShader.UMat4fv("proj", glm::value_ptr(proj));
-			subjectShader.UMat4fv("view", glm::value_ptr(view));
-			for (unsigned int i = 0; i < 10; i++)
-			{
-				model = glm::mat4(1.0f);
-				model = glm::translate(model, cubePositions[i]);
-				float angle = 20.0f * i;
-				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-				subjectShader.UMat4fv("model", glm::value_ptr(model));
-
-				glDrawArrays(GL_TRIANGLES, 0, 36);
-			}
-		}
-
-		{
-			light.Bind();
-			lightShader.Bind();
-
-			lightTexture.Bind(0);
-			lightShader.U1i("u_TextureColor", 0);
-
-			lightShader.UMat4fv("proj", glm::value_ptr(proj));
-			lightShader.UMat4fv("view", glm::value_ptr(view));
-			for (unsigned int i = 0; i < points; i++) {
-				model = glm::mat4(1.0f);
-				model = glm::translate(model, pointLightPositions[i]);
-				model = glm::scale(model, glm::vec3(0.2f));
-				lightShader.UMat4fv("model", glm::value_ptr(model));
-				glDrawArrays(GL_TRIANGLES, 0, 36);
-			}
-		}
+		shader.UMat4fv("proj", glm::value_ptr(proj));
+		shader.UMat4fv("view", glm::value_ptr(view));
+		shader.UMat4fv("model", glm::value_ptr(model));
+		
+		testModel.Draw(shader);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
